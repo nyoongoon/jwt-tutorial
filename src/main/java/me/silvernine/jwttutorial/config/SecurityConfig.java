9f584488659a8b3fecb31a329b924a3d7b3c31e6
nyoongoon <nyoongoon@naver.com>
2022-10-29 16:24:23 +0900

    jwt start

---
+++ src/main/java/me/silvernine/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package me.silvernine.jwttutorial.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity //기본 웹 보안 활성화 어노테이션
+public class SecurityConfig {
+
+}
